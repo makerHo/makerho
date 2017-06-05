eafe82d79851ab080e0911ea5b16c4fe6e1d7eb0
makerHo <524439109@qq.com>
2017-06-05 23:45:42 +0800

    2017年6月5日23:45:36
    2017年6月5日23:45:36

--- Myfood/js/index.js
+++ Myfood/js/index.js
@@ -3,16 +3,42 @@ window.addEventListener("load",function(){
 	var nav = document.querySelector(".nav");
 //	console.log(nav)
 	var scrollTop=getScrollSize().top;
-		console.log(scrollTop)
+//		console.log(scrollTop)
 	//scroll 是window的事件
 	window.addEventListener("scroll",function(){
 		if(scrollTop>56){
-			nav.className="navfixed";
-		}else{
 			nav.className="";
+		}else{
+			nav.className="navfixed";
 		}
 
 	})	
-/** window end*/
 
+/*--------banner loop-------------*/
+var imgObjs= document.querySelector(".barImgs img");
+var barImgs = document.querySelector(".barImgs")
+var banImgLis= document.querySelectorAll(".banImgLi li");
+//	console.log(banImgLis)
+	autoplay(1);
+	function autoplay(index){
+		timer=setInterval(function(){
+			barImgs.style.left=-imgObjs.offsetWidth*index+"px";
+				for(var i=0;i<banImgLis.length;i++){
+					
+					banImgLis[i].className="";
+				}
+//				console.log(i)
+				banImgLis[index].className="spotlight";
+				index++;
+				if(index==banImgLis.length){
+					index=0
+				}
+		},3000)
+	}
+	
+	
+	
+	
+	
+ /**----------------------------------- window end--------------------------------*/ 
 })
