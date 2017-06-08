2b1603e0af8236708be2a14126c6147c502ec6d8
makerHo <524439109@qq.com>
2017-06-08 10:15:31 +0800

    login 页面布局完成
    login 页面布局完成

--- Myfood/js/login.js
+++ Myfood/js/login.js
@@ -1,10 +1,26 @@
 window.addEventListener("load",function(){
+	//body   背景图随机切换
+var bodyImg = document.querySelector(".bodyImg")
+var timers = setInterval(function(){
+	var bodyImgRandom=Math.ceil(Math.random()*3);
+		bodyImg.src="img/login/"+bodyImgRandom+".jpg"
+},3000)
 	
-	
-	
-	
-	
-	
-	
+//secMidBox center
+var secMidBox = document.querySelector(".secMidBox");
+elementCenter(secMidBox);
+//ragister Tab
+var secMidBoxToplis = document.querySelectorAll(".secMidBoxTop li");
+var secMidBoxBtmlis = document.querySelectorAll(".secMidBoxBtm>ul>li");
+	secMidBoxToplis.forEach(function(value,index){
+		value.addEventListener("click",function(){
+			secMidBoxToplis.forEach(function(v,i){
+				v.className="";
+				secMidBoxBtmlis[i].className="";
+			});
+			value.className="secMidBoxTopLisClick";
+			secMidBoxBtmlis[index].className="regDisplay";
+		});
+	});
 /**----------------------------------------------*/
 })
